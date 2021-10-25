d51fc46b5faf5579e968ed66fa713fe61d29b5c7
codezwjava <1563330148@qq.com>
2021-10-25 13:51:45 +0800

    first commit

---
+++ src/main/java/com/xxxx/test1/test1.java
@@ -0,0 +1,20 @@
+package com.xxxx.test1;
+
+import javax.lang.model.element.VariableElement;
+import java.lang.reflect.Field;
+
+public class test1 {
+    /**
+     * 不改变原对象的引用
+     *  将“abc” 变为 “abcd”
+     * @param args
+     */
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+
+        String s = new String("abc");
+
+        s.getClass().getDeclaredField("value").set("value","abcd");
+
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/com/xxxx/test2/test1.java
@@ -0,0 +1,4 @@
+package com.xxxx.test2;
+
+public class test1 {
+}
