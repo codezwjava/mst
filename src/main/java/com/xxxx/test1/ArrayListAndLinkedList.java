8da8d8dcaf2785aba5345f8856132e0b1d755eb0
codezwjava <1563330148@qq.com>
2021-10-26 16:06:25 +0800

    "bug fixed"

--- src/main/java/com/xxxx/test1/ArrayListAndLinkedList.java
+++ src/main/java/com/xxxx/test1/ArrayListAndLinkedList.java
@@ -1,22 +1,20 @@
 package com.xxxx.test1;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 public class ArrayListAndLinkedList {
     //ArrayList和LinkedList有哪些区别
     public static void main(String[] args) {
-        ArrayList<String> arrayList = new ArrayList<>();//数组
-        arrayList.add("a");
-
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList("a","b","c","d"));//数组
         //指定下标位置添加元素  数据需要依次向后移动  比较消耗性能
-        arrayList.add(3,"d");
+        arrayList.add(2,"aa");
 
-        LinkedList<String> linkedList = new LinkedList<>();//链表
-        linkedList.add("a");
-
+        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList("a","b","c","d"));//链表
         //指定下标位置添加元素  需要遍历数组找到指定下标位置 添加元素
-        linkedList.add(3,"d");
+        linkedList.add(2,"aa");
 
         System.out.println(arrayList);
         System.out.println(linkedList);
